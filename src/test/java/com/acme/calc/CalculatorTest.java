c5d64939bace3753d012d6acd1397687326c0036
KWojtaszewski <karol.wojtaszewski@stazysta.comarch.pl>
2015-07-06 09:00:25 +0200

    Dodanie nowych metod w CalculatorTest

--- src/test/java/com/acme/calc/CalculatorTest.java
+++ src/test/java/com/acme/calc/CalculatorTest.java
@@ -1,20 +1,52 @@
 package com.acme.calc;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
 
-	private Calculator calculator;
+	private Calculator calculator = new Calculator();;
 
 	@Test
 	public void additionShouldReturnCorrectResult() {
-
+		// given
+		double firstNumber = 5.0;
+		double secondNumber = 6.0;
+		// when w osobnej metodzie
+		Double result = calculator.add(firstNumber, secondNumber);
+		// then w osobnej metodzie
+		Assert.assertTrue(result == 11);
 	}
 
 	@Test
 	public void subtractionShouldReturnCorrectResult() {
+		double firstNumber = 5.0;
+		double secondNumber = 6.0;
+		// when w osobnej metodzie
+		Double result = calculator.subtract(firstNumber, secondNumber);
+		// then w osobnej metodzie
+		Assert.assertTrue(result == -1);
 	}
 
-	// TODO division and multiplication test!
+	@Test
+	public void multiplicationShouldReturnCorrectResult() {
+		double firstNumber = 5.0;
+		double secondNumber = 6.0;
+		// when w osobnej metodzie
+		Double result = calculator.multiply(firstNumber, secondNumber);
+		// then w osobnej metodzie
+		Assert.assertTrue(result == 30);
+	}
+	
+	@Test
+	public void divideShouldReturnCorrectResult() {
+		double firstNumber = 5.0;
+		double secondNumber = 1.0;
+		// when w osobnej metodzie
+		Double result = calculator.divide(firstNumber, secondNumber);
+		// then w osobnej metodzie
+		Assert.assertTrue(result == 5);
+		
+	}
 
 }
